eb76ef4d5b33ac923d49b646fc3e3da5b2d39346
TruongMai <112840965+TruongMai98@users.noreply.github.com>
2022-11-25 22:59:40 +0700

    [Thực hành] Tạo trang web hiển thị thời gian hệ thống

---
+++ src/main/java/com/th/thtaotrangwebhienthithoigianhethong/ServerTimeServlet.java
@@ -0,0 +1,26 @@
+package com.th.thtaotrangwebhienthithoigianhethong;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "ServerTimeServlet", value = "/ServerTimeServlet")
+public class ServerTimeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Hello world"+ new Date() + "</h1>");
+        writer.println("</html>");
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
